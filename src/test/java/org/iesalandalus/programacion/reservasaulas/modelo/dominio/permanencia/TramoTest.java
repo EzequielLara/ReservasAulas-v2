8b82255384b38690ccde342bcc2787746c994076
Ezk24 <Ezk24@DESKTOP-O4DUUU6>
2019-02-23 01:37:55 +0100

    Traslado de paquete de clase Tramo.Pasa tests

--- src/test/java/org/iesalandalus/programacion/reservasaulas/modelo/dominio/permanencia/TramoTest.java
+++ src/test/java/org/iesalandalus/programacion/reservasaulas/modelo/dominio/permanencia/TramoTest.java
@@ -1,12 +1,12 @@
 package org.iesalandalus.programacion.reservasaulas.modelo.dominio.permanencia;
-/*
+
 import static org.junit.Assert.*;
 
 import org.iesalandalus.programacion.reservasaulas.modelo.dominio.permanencia.Tramo;
 import org.junit.Test;
-*/
+
 public class TramoTest {
-/*
+
 	@Test
 	public void identificadoresValidosTest() {
 		Tramo tramo;
@@ -27,5 +27,5 @@ public class TramoTest {
 		assertEquals("Mañana", Tramo.MANANA.toString());
 		assertEquals("Tarde", Tramo.TARDE.toString());
 	}
-*/
+
 }
