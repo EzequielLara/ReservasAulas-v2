92618339b54cc5c8c99b5fc5edc0ed6d13338bb4
Ezk24 <Ezk24@DESKTOP-O4DUUU6>
2019-03-04 11:13:38 +0100

    Implementación del método Main

--- src/main/java/org/iesalandalus/programacion/reservasaulas/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/MainApp.java
@@ -1,20 +1,21 @@
 package org.iesalandalus.programacion.reservasaulas;
 
-/*
+
 import org.iesalandalus.programacion.reservasaulas.controlador.ControladorReservasAulas;
 import org.iesalandalus.programacion.reservasaulas.controlador.IControladorReservasAulas;
-import org.iesalandalus.programacion.reservasaulas.modelo.IModeloReservasAulas;
-import org.iesalandalus.programacion.reservasaulas.modelo.ModeloReservasAulas;
+import org.iesalandalus.programacion.reservasaulas.modelo2.IModeloReservasAulas;
+import org.iesalandalus.programacion.reservasaulas.modelo2.ModeloReservasAulas;
+
 import org.iesalandalus.programacion.reservasaulas.vista.IVistaReservasAulas;
 import org.iesalandalus.programacion.reservasaulas.vista.VistaReservasAulas;
-*/
+
 public class MainApp {
 
 	public static void main(String[] args) {
-		/*IVistaReservasAulas vista = new VistaReservasAulas();
+		IVistaReservasAulas vista = new VistaReservasAulas();
 		IModeloReservasAulas modelo = new ModeloReservasAulas();
-		IControladorReservasAulas controlador = new ControladorReservasAulas(modelo, vista);
-		controlador.comenzar();*/
+		IControladorReservasAulas controlador = new ControladorReservasAulas(modelo, (VistaReservasAulas) vista);
+		controlador.comenzar();
 	}
 
 }
